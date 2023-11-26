f5f8fbe35e86cffce508c1bd91874132be2afd92
Ricardo Vieira <franciscorvpinheiro49@gmail.com>
2023-11-26 12:27:06 -0300

    Working JWT Token Creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
-import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
@@ -7,12 +7,12 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    AuthModule,
-    TodosModule,
-    UsersModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
+    TodosModule,
+    UsersModule,
     MongooseModule.forRoot(process.env.MONGODB_DEV_STRING),
   ],
   controllers: [],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from 'src/users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from 'src/users/users.service';
 import { UsersController } from 'src/users/users.controller';
 import { UserSchema } from 'src/schemas/user.schema';
 import { JwtModule } from '@nestjs/jwt';
-
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UsersModule,
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
     JwtModule.register({
       global: true,
-      secret: `${process.env.JWT_SECRET}`,
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '10d' },
     }),
   ],
